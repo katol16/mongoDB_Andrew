80c594ef3c3eb455b7d0632cdcbd4e5ac9d71274
karol <karol.vogelgezang@gmail.comgit config --global user.email karol.vogelgezang@gmail.com>
2018-12-17 16:26:27 +0100

    cos sie jebie z testami

---
+++ node-todo-api/server/tests/server.test.js
@@ -0,0 +1,54 @@
+const expect = require('expect');
+const request = require('supertest');
+
+const {app} = require('./../server');
+const {Todo} = require('./../models/todo');
+
+// beforeEach wykona jakąś akcje, przed rozpoczęciem testów, tutaj wyczyści nam bazę (tylko testowo)
+beforeEach((done) => {
+   Todo.remove({}).then(() => done());
+});
+
+describe('POST /todos', () => {
+    it('should create a new todo', (done) => {
+        var text = 'Test todo text';
+
+        request(app)
+            .post('/todos')
+            // dzieki supertest, w send() stworzymy obiekt ze zmiennej text
+            .send({text})
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.text).toBe(text);
+            })
+            .end((err, res) => {
+                if (err) {
+                    return done(err);
+                }
+
+                Todo.find().then((todos) => {
+                    // tutaj jest załozenie, ze baza jest pusta i beforeEach, nam czysci bazę (tylko testowo)
+                    expect(todos.length).toBe(1);
+                    expect(todos[0].text).toBe(text);
+                    done();
+                }).catch((e) => done(e));
+            });
+    });
+
+    it('should not create todo with invalid body data', (done) => {
+        request(app)
+            .post('/todos')
+            .send({})
+            .expect(400)
+            .end((err, res) => {
+                if (err) {
+                    return done(err);
+                }
+
+                Todo.find().then((todos) => {
+                    expect(todos.length).toBe(0);
+                    done();
+                }).catch((e) => done(e));
+            });
+    });
+});
